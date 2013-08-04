294599dd99fbe4837cdf833972893e667431e2f0
Yue Lu <hacklu.newborn@gmail.com>
2013-08-04 04:58:20 +0000

    fix PATH_MAX macro not defined

---
+++ gdb/gdbserver/gnu-low.c
@@ -0,0 +1,7 @@
+void test(){}
+
+int using_threads=0;
+void initialize_low(void)
+{
+
+}
--- gdb/gdbserver/hostio.c
+++ gdb/gdbserver/hostio.c
@@ -25,6 +25,10 @@
 #include <limits.h>
 #include <unistd.h>
 
+#ifndef PATH_MAX 
+#define PATH_MAX 512
+#endif
+
 extern int remote_debug;
 
 struct fd_list
